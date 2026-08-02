ae36e1003b3c03c310df611b4233a0ee94f0bc99
Kodigudla Harshith <kharshith849@gmail.com>
2026-08-03 01:54:59 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,31 +3,42 @@
 while True:
     print("\n===== CALCULATOR =====")
 
-    try:
-        num1 = float(input("Enter first number: "))
-        operator = input("Enter operator (+, -, *, /): ")
-        num2 = float(input("Enter second number: "))
+    num1 = float(input("Enter first number: "))
+    operator = input("Enter operator (+, -, *, /, //, %, **): ")
+    num2 = float(input("Enter second number: "))
 
-        if operator == "+":
-            print("Result =", num1 + num2)
+    if operator == "+":
+        print("Result =", num1 + num2)
 
-        elif operator == "-":
-            print("Result =", num1 - num2)
+    elif operator == "-":
+        print("Result =", num1 - num2)
 
-        elif operator == "*":
-            print("Result =", num1 * num2)
+    elif operator == "*":
+        print("Result =", num1 * num2)
 
-        elif operator == "/":
-            if num2 != 0:
-                print("Result =", num1 / num2)
-            else:
-                print("Error: Cannot divide by zero!")
-
+    elif operator == "/":
+        if num2 != 0:
+            print("Result =", num1 / num2)
         else:
-            print("Invalid operator!")
+            print("Error: Cannot divide by zero!")
 
-    except ValueError:
-        print("Please enter valid numbers!")
+    elif operator == "//":
+        if num2 != 0:
+            print("Result =", num1 // num2)
+        else:
+            print("Error: Cannot divide by zero!")
+
+    elif operator == "%":
+        if num2 != 0:
+            print("Result =", num1 % num2)
+        else:
+            print("Error: Cannot divide by zero!")
+
+    elif operator == "**":
+        print("Result =", num1 ** num2)
+
+    else:
+        print("Invalid operator!")
 
     choice = input("\nDo another calculation? (y/n): ")
 
